d126b6459a4d3c5a45efe59e0cc33e95a01ccf82
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2024-10-10 23:23:16 +0530

    10/10/24

--- List_Comprehension_learning.py
+++ List_Comprehension_learning.py
@@ -1,2 +1,21 @@
-a =[i*2 for i in [1,2,5]]
-print(a)  
+num = [1,2,5]
+
+# num =[i*2 for i in [1,2,5]]
+# print(num)  
+
+# def timeSix(num):
+#     return num * 2
+
+# result = [timeSix(i) for i in num] #if i > 2]
+# print(result)
+
+string = ["intro", "to", "list", "comprehension"]
+dicts = [{"name":"John"}, {"name":"henry"}]
+
+# result = []
+# for i in dicts:
+#     result.append(i['name'])
+#     print(result)
+
+result = [i['name'] for i in dicts]
+print(result)   
