eb5866f0167ccf2e45ac455fb4e0978735f4814b
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2024-10-10 22:25:47 +0530

    10/10/24

---
+++ List_Comprehension_learning.py
@@ -0,0 +1,2 @@
+a =[i*2 for i in [1,2,5]]
+print(a)  
--- file_handling.py
+++ file_handling.py
@@ -1,5 +1,4 @@
 # open to open file and read file 
 # it is a build in function 
-print(open('hello.py','r').read())
-
-with (open('hello.py','r').read())
+print(open('hello.py','r').read()) 
+with(open('hello.py','r').read())
